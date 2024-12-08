58eedead3d7aa45445d686c8b914a47a89181dea
Yuwei Sun <ys4680@nyu.edu>
2024-12-08 03:04:57 -0500

    clean Tasks

--- job-test/run_threads.sh
+++ job-test/run_threads.sh
@@ -4,26 +4,19 @@
 run_thread() {
     local thread_id=$1
     echo "Thread $thread_id is running"
-    sleep 10
+    sleep 10  # Simulate some work
     echo "Thread $thread_id has finished"
 }
 
-max_threads=$(ulimit -u)
-available_threads=$((max_threads - 2))  # Reserve extra thread
-
-# limit thread
-num_threads=4
-if [[ $num_threads -gt $available_threads ]]; then
-    num_threads=$available_threads
-    echo "Adjusting number of threads to: $num_threads"
-fi
+# Number of threads
+num_threads=20
 
 # Start threads
 for ((i=1; i<=num_threads; i++)); do
-    run_thread $i &
-    sleep 0.1 
+    run_thread $i &  # Run the function in the background
 done
 
 # Wait for all threads to finish
 wait
+
 echo "All threads have completed"
